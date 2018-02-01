cbd53b3860e671777af9a658c2dda465fa7b1be0
Wudi <wudi.andy@qq.com>
2018-02-01 21:23:46 +0800

    当鼠标在液柱上的时候改变液柱的颜色

--- index.js
+++ index.js
@@ -1,33 +1,71 @@
 window.onload = function() {
     "use strict";
 
-    // 变量统一定义
+    var myCanvas = document.getElementById("myCanvas");
+
+    // 设置canvas的宽度为浏览器的宽度
+    // myCanvas.style.width = window.innerWidth-30+"px";
+    // myCanvas.style.height = window.innerHeight-30+"px";
+
+    // 定义图表属性类
+    function ChartProperty(){}
+    var chartProperty = new ChartProperty();
+    chartProperty.actualValue = 0.3; // 实际值, 即液柱显示的值
+    chartProperty.plannedValue = 0.4; // 计划值, 即三角指向的值
+    chartProperty.isMouseOver = false; // 鼠标是否在液柱上, true: 在液柱上, false: 不在液柱上
+    
     var current = 300; // 液柱当前的值
     var index2 = 330; // 三角形指向的值
 
     // 取得canvas context对象
-    var myCanvasContext = document.getElementById("myCanvas").getContext("2d");
+    var myCanvasContext = myCanvas.getContext("2d");
     myCanvasContext.save();
 
     // 动画效果
     var index = 0;
+    var endValue = chartProperty.actualValue;
     var id = window.setInterval(function(){
         myCanvasContext.clearRect(0, 0, myCanvasContext.width, myCanvasContext.height);
-        draw(myCanvasContext, index, index2);
-        if (index >= current) {
+        chartProperty.actualValue = index;
+        draw(myCanvasContext, chartProperty);
+        if (index >= endValue) {
             window.clearInterval(id);
         }
-        index++;
-    }, 10);
+        index += 0.01;
+    }, 30);
+
+    // 鼠标事件
+    myCanvas.addEventListener("mousemove", function(event){
+        // 判断鼠标是否在液柱上
+        // 获取鼠标坐标
+        var x = event.clientX - myCanvas.offsetLeft;
+        var y = event.clientY - myCanvas.offsetTop;
+        
+        if (x>27 && x<369 && y>142 && y<157) { // 如果鼠标在液柱上, 重新绘制图表
+            chartProperty.isMouseOver = true;
+            chartProperty.mouseX = x;
+            chartProperty.mouseY = y;
+            myCanvasContext.clearRect(0, 0, myCanvasContext.width, myCanvasContext.height);
+            draw(myCanvasContext, chartProperty);
+        }else{
+            if (chartProperty.isMouseOver == true) { // 如果鼠标从液柱上移开, 重新绘制图表
+                chartProperty.isMouseOver = false;
+                myCanvasContext.clearRect(0, 0, myCanvasContext.width, myCanvasContext.height);
+                draw(myCanvasContext, chartProperty);
+            }
+        }
+    });
 };
 
 /**
  * 画出温度计的一帧
- * @param {Object} myCanvasContext <canvas> DOM对象
- * @param {Number} index 液柱的偏移量
- * @param {Number} index2 三角形的偏移量
+ * @param {Object} myCanvasContext canvas context对象
+ * @param {Object} chartProperty 图表属性对象
  */
-function draw(myCanvasContext, index, index2) {
+function draw(myCanvasContext, chartProperty) {
+    var index = chartProperty.actualValue*342;
+    var index2 = chartProperty.plannedValue*342;
+
     // 参数验证
     if (!myCanvasContext) return;
     if (index < 0) index = 0;
@@ -86,10 +124,17 @@ function draw(myCanvasContext, index, index2) {
     myCanvasContext.restore();
     myCanvasContext.save();
     linearGradient = myCanvasContext.createLinearGradient(0, 144, 0, 157);
-    linearGradient.addColorStop(0, 'rgb(255, 242, 31)');
-    linearGradient.addColorStop(0.3, 'rgb(252, 205, 0)');
-    linearGradient.addColorStop(0.6, 'rgb(241, 148, 46)');
-    linearGradient.addColorStop(1, 'rgb(255, 126, 86)');
+    if(chartProperty.isMouseOver) {
+        linearGradient.addColorStop(0, 'rgb(235, 222, 11)');
+        linearGradient.addColorStop(0.3, 'rgb(232, 185, 0)');
+        linearGradient.addColorStop(0.6, 'rgb(221, 128, 26)');
+        linearGradient.addColorStop(1, 'rgb(235, 106, 66)');
+    }else{
+        linearGradient.addColorStop(0, 'rgb(255, 242, 31)');
+        linearGradient.addColorStop(0.3, 'rgb(252, 205, 0)');
+        linearGradient.addColorStop(0.6, 'rgb(241, 148, 46)');
+        linearGradient.addColorStop(1, 'rgb(255, 126, 86)');
+    }
     myCanvasContext.fillStyle = linearGradient;
 
     // 形状
@@ -113,11 +158,11 @@ function draw(myCanvasContext, index, index2) {
     myCanvasContext.fillStyle = "rgb(16, 128, 243)";
 
     // 形状
-    // 0%: 27, 100%: 369
+    // 0%: 0, 100%: 342
     myCanvasContext.beginPath();
-    myCanvasContext.moveTo(index2-8, 120);
-    myCanvasContext.lineTo(index2+8, 120);
-    myCanvasContext.lineTo(index2, 129);
+    myCanvasContext.moveTo(27+index2-8, 120);
+    myCanvasContext.lineTo(27+index2+8, 120);
+    myCanvasContext.lineTo(27+index2, 129);
     myCanvasContext.fill();
 
     // Layer 5 刻度
@@ -129,17 +174,11 @@ function draw(myCanvasContext, index, index2) {
 
     // 形状
     for (var i = 0; i <= 100; i+=2) {
-        /**
-         * 刻度线的高度
-         */
+        // 刻度线的高度
         var scopeHeight = 0;
-        /**
-         * 上刻度线基准高度
-         */
+        // 上刻度线基准高度
         var upScopeBaseHeight = 142;
-        /**
-         * 下刻度线基准高度
-         */
+        // 下刻度线基准高度
         var downScopeBaseHeight = 157;
 
         // 计算刻度线的高度
@@ -169,5 +208,14 @@ function draw(myCanvasContext, index, index2) {
         myCanvasContext.lineTo(36+i*3.42, downScopeBaseHeight);
         myCanvasContext.closePath();
         myCanvasContext.fill();
+
+        // Layer 6 提示框
+        myCanvasContext.restore();
+        myCanvasContext.save();
+
+        // 颜色
+        // 形状
+        myCanvasContext.beginPath();
+        myCanvasContext.moveTo();
     }
 }
